3ea23ad205ac2ddc6f4cc2d39f3509eade987b05
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-16 10:18:43 -0700

    Added more task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,12 +11,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0) {
+	if (n > 0)
 	  puts("is positive");
-	} else if (n == 0) {
+	else if (n == 0)
 	  puts("is zero");
-	} else {
+        else
 	  puts("is negative");
-	}
+	
 	return (0);
 }
