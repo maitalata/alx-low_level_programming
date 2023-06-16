c5066604d3d23a96d63ecd14819a09cad48c1ee1
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-16 15:43:08 -0700

    Added more task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * This function print alphabets
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'e' || letter == 'q')
+		{
+			continue;
+		}
+
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
