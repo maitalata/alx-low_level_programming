263bbf4dd39bd79d20250b9059d0f66b39209ddd
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-16 15:33:50 -0700

    Added more task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * This function print alphabets
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
