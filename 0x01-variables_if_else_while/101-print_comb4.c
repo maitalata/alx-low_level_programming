4d2d80c9eac9c9640e8ac346755e85c7d8497359
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-16 17:09:37 -0700

    Added more task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,35 +10,34 @@
  */
 int main(void)
 {
-    int digit1, digit2, digit3;
-    int firstCombination = 1;
-
-    for (digit1 = 0; digit1 <= 9; digit1++)
-    {
-        for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
-        {
-            for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
-            {
-                if (firstCombination)
-                {
-                    firstCombination = 0;
-                }
-                else
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-
-                putchar(digit1 + '0');
-                putchar(digit2 + '0');
-                putchar(digit3 + '0');
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+	int digit1, digit2, digit3;
+	int firstCombination = 1;
+	
+	for (digit1 = 0; digit1 <= 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
+			{
+				if (firstCombination)
+				{
+					firstCombination = 0;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+				putchar(digit3 + '0');
+			}
+		}
+	}
+	
+	putchar('\n');
+	
+	return (0);
 }
-
 
