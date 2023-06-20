aef2300d17438e412cde94092a8ca01711254b06
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-20 15:07:45 -0700

    Added program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point for the program
+ *
+ * This function prints puthcar
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
