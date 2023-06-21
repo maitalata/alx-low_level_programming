20c52149c4bee30cd1e877e6ece71122ce235551
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-21 13:26:31 -0700

    Added Another Task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,14 +10,14 @@ void print_times_table(int n)
 	if (n <= 15 && n >= 0)
 	{
 		int i, j;
-		
+
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				printf("%4d", i * j);
 			}
-			
+
 			printf("\n");
 		}
 	}
