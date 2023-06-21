823a6033a6327564c7e5a1600b502bfb713f4c03
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-21 12:12:38 -0700

    Added Another Task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Function to print signs
+ *
+ * @n: The sign number
+ *
+ * Return: int number indicating success or otherwise
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
