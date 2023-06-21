58c84531a4b7641758b0400c5be2474cb95ca9b0
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-21 12:33:48 -0700

    Added Another Task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Function to get absolute values
+ *
+ * @r: The number
+ *
+ * Return: int number indicating success or otherwise
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
