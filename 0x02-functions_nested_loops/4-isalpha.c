fdf56dace4a0f282c9eebf3339ba7cc89cf33101
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-21 12:06:26 -0700

    Added Another Task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Function to check if character is lower
+ * or alphabet
+ *
+ * @c: The character to print
+ *
+ * Return: int number indicating success or otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
