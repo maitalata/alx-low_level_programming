8098032ba2da8c3145649de59ec124a621fba02a
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-21 12:53:43 -0700

    Added Another Task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Function to add two numbers
+ * @x: first number to add
+ * @y: Second number to add
+ * Return: the sum of the numbers
+ */
+int add(int x, int y)
+{
+	int n;
+	 n = i + l;
+	return (n);
+}
