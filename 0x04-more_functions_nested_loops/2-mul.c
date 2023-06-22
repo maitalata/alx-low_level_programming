2e5b08537f316d09f47fc1660ecef3b5cb2c080e
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-22 02:53:39 -0700

    Added Another Task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply two number
+ *
+ * a: the first number to multiply
+ * b: the second number to multiply
+ *
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
