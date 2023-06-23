e047b1e1bca31420aef2c4a28a67d20a2d8e77ae
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-22 19:58:16 -0700

    Added Another Task

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,8 +3,8 @@
 /**
  * mul - multiply two number
  *
- * a: the first number to multiply
- * b: the second number to multiply
+ * @a: the first number to multiply
+ * @b: the second number to multiply
  *
  * Return: the product of a and b
  */
