926bcbcef8d802d40c33290fc6b70215c557f2ea
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-23 00:28:58 -0700

    Added Another Task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,10 +5,10 @@
  * starting with 0.
  * @n: The value of the times table to be printed.
  */
-void print_times_table(int n) 
+void print_times_table(int n)
 {
 	int num, mult, prod;
-	
+
 	if (n >= 0 && n <= 15)
 	{
 		for (num = 0; num <= n; num++)
@@ -26,12 +26,12 @@ void print_times_table(int n)
 				if (prod <= 9)
 					_putchar(' ');
 
-				if (prod >= 100) 
+				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
 					_putchar((prod / 10) % 10 + '0');
 				}
-				else if(prod <= 99 && prod >= 10)
+				else if (prod <= 99 && prod >= 10)
 				{
 					_putchar((prod / 10) + '0');
 				}
