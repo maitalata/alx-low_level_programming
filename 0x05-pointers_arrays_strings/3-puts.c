dd94705e9f95a93c7a1591e4ae0e49e751e68308
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-26 10:41:49 -0700

    Added Another Task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - print a given string
+ *
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(star[i++]);
+	}
+
+	_putchar('\n');
+}
+
