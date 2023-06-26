38651eefa466cdd0b49304adcc9bede5da0ccdce
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-26 09:58:11 -0700

    Added Another Task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen - function to determine the 
+ * _strlen - function to determine the
  * length of a string
  *
  * @s: the string to act upon
@@ -13,10 +13,11 @@ int _strlen(char *s)
 	int length;
 
 	length = 0;
-	while(s[length] != '\0')
+	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	return length;
+	return (length);
 }
+
