f9697d55f4eb788c3b67ba0f578563be12755c5a
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-26 09:25:29 -0700

    Added Another Task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swapt to numbers/integers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}	
