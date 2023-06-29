9cca07c537433b63872e5e5cba3134a64e155eb9
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-06-29 11:39:58 -0700

    Added New Task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
+/**
+ * _strcat - concatenate two strings together
+ *
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return - the two strings concatenated
+ * together
+ */
 char *_strcat(char *dest, char *src)
 {
 	int dest_length, i;
 
 	dest_length = 0;
 	i = 0;
+
 	while (dest[dest_length] != '\0')
 	{
 		dest_length++;
@@ -24,3 +34,4 @@ char *_strcat(char *dest, char *src)
 
 	return dest;
 }
+
